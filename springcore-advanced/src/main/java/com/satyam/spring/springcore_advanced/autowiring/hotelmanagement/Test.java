534e28cd66096464e53a22b4de30cdafc5f19c31
naivedeveloper95 <findsatyam95@gmail.com>
2023-12-10 01:03:41 +0530

    Added Autowiring

---
+++ springcore-advanced/src/main/java/com/satyam/spring/springcore_advanced/autowiring/hotelmanagement/Test.java
@@ -0,0 +1,15 @@
+package com.satyam.spring.springcore_advanced.autowiring.hotelmanagement;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class Test {
+
+	public static void main(String[] args) {
+		ApplicationContext context = new ClassPathXmlApplicationContext(
+				"com/satyam/spring/springcore_advanced/autowiring/annotations/config.xml");
+
+		Employee employee = (Employee) context.getBean("employee");
+		System.out.println(employee);
+	}
+}
